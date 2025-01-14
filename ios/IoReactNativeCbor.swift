0686dab54d9259fa494d3a4027e1f04eea07454d
Federico Mastrini <federicomastrini93@gmail.com>
2025-01-14 21:32:38 +0100

    chore: initial commit

---
+++ ios/IoReactNativeCbor.swift
@@ -0,0 +1,8 @@
+@objc(IoReactNativeCbor)
+class IoReactNativeCbor: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
