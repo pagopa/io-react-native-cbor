43352132cd5aec575e744707705f922ae882baa2
Federico Mastrini <federicomastrini93@gmail.com>
2025-01-27 11:07:33 +0100

    chore: clean android example

--- example/android/app/src/main/java/pagopa/ioreactnativecbor/example/MainActivity.kt
+++ example/android/app/src/main/java/pagopa/ioreactnativecbor/example/MainActivity.kt
@@ -1,6 +1,5 @@
 package pagopa.ioreactnativecbor.example
 
-import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
