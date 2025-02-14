d73014ae6ec4e82913532c5c843290ec0bd2c39b
hevelius <mario.perrotta@pagopa.it>
2025-02-14 11:11:50 +0100

    feat: add local xcframework of cbor-ios

---
+++ cbor.xcframework/ios-arm64_x86_64-simulator/cbor.framework/Headers/cbor.h
@@ -0,0 +1,18 @@
+//
+//  cbor.h
+//  cbor
+//
+//  Created by Antonio on 02/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for cbor.
+FOUNDATION_EXPORT double cborVersionNumber;
+
+//! Project version string for cbor.
+FOUNDATION_EXPORT const unsigned char cborVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cbor/PublicHeader.h>
+
+
--- ios/IoReactNativeCbor.swift
+++ ios/IoReactNativeCbor.swift
@@ -1,8 +1,11 @@
+import cbor
+
 @objc(IoReactNativeCbor)
 class IoReactNativeCbor: NSObject {
 
   @objc(multiply:withB:withResolver:withRejecter:)
   func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
     resolve(a*b)
+    let test = CoseKey(jwk: "")
   }
 }
